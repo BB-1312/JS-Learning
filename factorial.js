f7936266f8b36b84f58d3424bb9faf2938cc3894
Lord-BB <117069495+Lord-BB@users.noreply.github.com>
2022-10-31 13:34:47 +0530

    Create factorial.js
    add factorial program

---
+++ factorial.js
@@ -0,0 +1,18 @@
+var number = 2
+if (number < 0) {
+    console.log('Error');
+}
+
+// if number is 0
+else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
+
+// if number is positive
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(fact);
+}
